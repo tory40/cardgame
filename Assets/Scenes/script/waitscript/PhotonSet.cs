2578730ff4a47b70cb8ea80fb7e4b47799fe9c10
tory40 <zn072358@gmail.com>
2022-12-11 18:38:29 +0900

    コードの追加

--- Assets/Scenes/script/waitscript/PhotonSet.cs
+++ Assets/Scenes/script/waitscript/PhotonSet.cs
@@ -6,36 +6,23 @@ using UnityEngine.SceneManagement;
 // MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
 public class PhotonSet : MonoBehaviourPunCallbacks
 {
-    public int input;
-    GameManager gameManager;
-    public void Wait()
+    string roomtype;
+    public void Wait(string type)
     {
         // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
+        roomtype = type;
         PhotonNetwork.ConnectUsingSettings();
     }
-    private void Update()
-    {
-    }
+        private void Update()
+        {
+
+        }
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
     public override void OnConnectedToMaster()
     {
         // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
         // 退出時にも呼ばれる
-        switch(input)
-        {
-            case 1:
-                PhotonNetwork.JoinOrCreateRoom("Free", new RoomOptions(), TypedLobby.Default);
-                break;
-            case 2:
-                PhotonNetwork.JoinOrCreateRoom("Rank", new RoomOptions(), TypedLobby.Default);
-                break;
-            case 3:
-                PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
-                break;
-            default:
-                return;
-
-        }
+        PhotonNetwork.JoinOrCreateRoom(roomtype, new RoomOptions(), TypedLobby.Default);
     }
 
     // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
